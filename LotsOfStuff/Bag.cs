113887cdc344e6c804a7e0501acbb924713b2171
Nuno <nunosilvares@hotmail.com>
2018-05-10 12:16:27 +0100

    Actualização da Bag para herdar métodos da List<T>

--- LotsOfStuff/Bag.cs
+++ LotsOfStuff/Bag.cs
@@ -4,18 +4,16 @@ using System.Collections.Generic;
 namespace Aula10
 {
     /// <summary>Classe que representa uma mochila ou saco que contem itens</summary>
-    public class Bag : IStuff
+    public class Bag : List<IStuff>, IStuff
     {
         public float Value
         {
             get
             {
                 float totalValue = 0;
-                foreach (IStuff s in stuff)
+                foreach (IStuff s in this)
                 {
-                    if (s != null) {
-                        totalValue += s.Value;
-                    }
+                    totalValue += s.Value;
                 }
                 return totalValue;
             }
@@ -25,54 +23,24 @@ namespace Aula10
             get
             {
                 float totalWeight = 0;
-                foreach (IStuff s in stuff)
+                foreach (IStuff s in this)
                 {
-                    if (s != null)
-                    {
-                        totalWeight += s.Weight;
-                    }
+                    totalWeight += s.Weight;
                 }
                 return totalWeight;
             }
         }
 
-        /// <summary>Array que contém os itens da mochila</summary>
-        private List<IStuff> stuff;
-
-        /// <summary>Número de itens na mochila</summary>
-        public int StuffCount { get { return stuff.Count; } }
-
         /// <summary>Construtor que cria uma nova instância de mochila</summary>
         /// <param name="bagSize">Número máximo de itens que é possível colocar na mochila</param>
-        public Bag(int bagSize)
+        public Bag(int bagSize) : base(bagSize)
         {
-            stuff = new List<IStuff>(bagSize);
-        }
 
-        /// <summary>Colocar um item na mochila</summary>
-        /// <param name="aThing">Item a colocar na mochila</param>
-        public void AddThing(IStuff aThing)
-        {
-            // Adicionar o item à mochila e depois incrementar o
-            // número de coisas na mochila
-            stuff.Add(aThing);
         }
-
-        /// <summary>Observar um item da mochila sem o remover da mesma</summary>
-        /// <param name="index">Local onde está o item a observar</param>
-        /// <returns>Item a ser observado</returns>
-        public IStuff GetThing(int index)
-        {
-            if (index > stuff.Count)
-            {
-                // Senão existir um item no local indicado, "lançar" uma exceção
-                throw new InvalidOperationException("Bag doesn't have that much stuff!");
-            }
-            return stuff[index];
-        }
+        
         public override string ToString()
         {
-            string resultado = "Bag\n\tNumber of Items: " + StuffCount.ToString() +
+            string resultado = "Bag\n\tNumber of Items: " + Count.ToString() +
                 "\n\tTotal Weight: " + Weight.ToString() + "\n\tTotal Value: " + Value.ToString();
             return resultado;
         }
--- LotsOfStuff/Program.cs
+++ LotsOfStuff/Program.cs
@@ -29,33 +29,33 @@ namespace Aula10
             //
 
             // Pão com 2 dias, 500 gramas
-            p.BagOfStuff.AddThing(new Food(FoodType.Bread, 2, 0.500f));
+            p.BagOfStuff.Add(new Food(FoodType.Bread, 2, 0.500f));
             // 300 gramas de vegetais com 5 dias
-            p.BagOfStuff.AddThing(new Food(FoodType.Vegetables, 5, 0.300f));
+            p.BagOfStuff.Add(new Food(FoodType.Vegetables, 5, 0.300f));
             // Pistola com 1.5kg + 50 gramas por bala, carregada com 10 balas, com um custo de 250€
-            p.BagOfStuff.AddThing(new Gun(1.5f, 0.050f, 10, 250));
+            p.BagOfStuff.Add(new Gun(1.5f, 0.050f, 10, 250));
             // 200 gramas de fruta fresca
-            p.BagOfStuff.AddThing(new Food(FoodType.Fruit, 0, 0.200f));
+            p.BagOfStuff.Add(new Food(FoodType.Fruit, 0, 0.200f));
 
             Console.WriteLine("\n(Before 2º bag) " + p.BagOfStuff);
 
             // Novo bag
             otherbag = new Bag(5);
             // 1kg de fruta com 1 dia
-            otherbag.AddThing(new Food(FoodType.Meat, 1, 1f));
+            otherbag.Add(new Food(FoodType.Meat, 1, 1f));
             // 500 gramas de fruta com 2 dias
-            otherbag.AddThing(new Food(FoodType.Vegetables, 2, 0.5f));
+            otherbag.Add(new Food(FoodType.Vegetables, 2, 0.5f));
 
             // Adicionar o otherbag à bag do jogador
-            p.BagOfStuff.AddThing(otherbag);
+            p.BagOfStuff.Add(otherbag);
 
             // Quantos itens tem o jogador na mochila?
             Console.WriteLine(p.BagOfStuff);
             // Percorrer itens na mochila e tentar "imprimir" cada um
-            for (int i = 0; i < p.BagOfStuff.StuffCount; i++)
+            foreach (IStuff aThing in p.BagOfStuff)
             {
-                IStuff aThing = p.BagOfStuff.GetThing(i);
                 Console.WriteLine("\n\t" + aThing);
+
                 if (aThing is Gun)
                 {
                     (aThing as Gun).Shoot();
